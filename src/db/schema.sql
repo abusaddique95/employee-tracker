69ebc8708768b580e478bf3741528bbc017dfe3e
abusaddique95 <abusaddique95@live.com>
2022-07-01 00:52:45 +0100

    added questions to be asked

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY NOT NULL, 
+    id INT NOT NULL, 
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
@@ -20,13 +20,13 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employees (
-id INT PRIMARY KEY NOT NULL,
+id INT NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
 manager_id INT,
-CONSTRAINT fkRole FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
-  CONSTRAINT fkManager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL,
+CONSTRAINT fkRole FOREIGN KEY (role_id) REFERENCES roles(id),
+  CONSTRAINT fkManager FOREIGN KEY (manager_id) REFERENCES employees(id),
   PRIMARY KEY (id)
 
 );
---
+++ utils/questions.js
@@ -0,0 +1,61 @@
+const questions = {
+  type: "list",
+  message: "Please select a category:",
+  name: "category",
+  choices: [
+    {
+      value: "viewDepartments",
+      name: "View All Departments",
+    },
+    {
+      value: "viewAllRoles",
+      name: "View All Roles",
+    },
+    {
+      value: "viewAllEmployees",
+      name: "View All Employees",
+    },
+    {
+      value: "addDepartment",
+      name: "Add a Department",
+    },
+    {
+      value: "addRole",
+      name: "Add a Role",
+    },
+    {
+      value: "addEmployee",
+      name: "Add an Employee",
+    },
+    {
+      value: "updateEmployee",
+      name: "Update an Employee's Manager",
+    },
+    {
+      value: "viewEmployeesByDepartment",
+      name: "View All Employees by Department",
+    },
+    {
+      value: "viewEmployeesByManager",
+      name: "View All Employees by Manager",
+    },
+    {
+      value: "removeDepartment",
+      name: "Remove a Department",
+    },
+    {
+      value: "removeRole",
+      name: "Remove a Role",
+    },
+    {
+      value: "removeEmployee",
+      name: "Remove an Employee",
+    },
+    {
+      value: "quit",
+      name: "Quit application",
+    },
+  ],
+};
+
+module.exports = { questions };
