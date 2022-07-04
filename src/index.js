249e21b5880ed61823b329abb46082a4bc19ae15
abusaddique95 <abusaddique95@live.com>
2022-07-04 18:35:44 +0100

    employees load, departments add and can now add a role

--- src/index.js
+++ src/index.js
@@ -45,18 +45,58 @@ const init = async () => {
       const roles = await executeQuery("SELECT * FROM roles");
       console.table(roles);
     }
-    if (selection === "view all employees") {
+    if (selection === "View all employees") {
       // const employees = await executeQuery(`SELECT * FROM employees`);
       // console.table(employees);
-      const query = "SELECT * FROM employees";
+      // const query = `SELECT  *
+      // FROM employees`;
+      const query = `SELECT  employees.id, firstName, lastName, title, salary, departments.name
+        FROM employees
+        INNER JOIN roles
+        ON employees.roleId=roles.id
+        LEFT JOIN departments
+        ON roles.departmentId = departments.id`;
+
       const data = await executeQuery(query);
       console.table(data);
     }
-    if (selection === "addDepartment") {
+    if (selection === "Add a department") {
       const addQuery = await inquirer.prompt(addDepartment);
-      const query = `INSERT INTO departments (name) VALUES ("${addQuery.name}")`;
+      const query = `INSERT INTO departments (name) VALUES ("${addQuery.departmentName}")`;
       await executeQuery(query);
       console.log("new department created");
+    }
+    if (selection === "Add a role") {
+      const queryOne = `SELECT * FROM departments;`;
+      const result = await executeQuery(queryOne);
+
+      const arr = result.map((department) => ({
+        name: department.name,
+        value: department.id,
+      }));
+
+      const answer = await inquirer.prompt([
+        {
+          type: "input",
+          name: "newTitle",
+          message: "What is this role's title?",
+        },
+        {
+          type: "input",
+          name: "newSalary",
+          message: "What is this role's salary?",
+        },
+        {
+          type: "list",
+          name: "deptId",
+          message: "What is this role's department that it is associated with?",
+          choices: arr,
+        },
+      ]);
+
+      console.log(answer);
+      const addingRole = `INSERT INTO roles (title, salary, departmentId) VALUES ("${answer.newTitle}", "${answer.newSalary}", "${answer.deptId}")`;
+      await executeQuery(addingRole);
     }
 
     // if (selection === "addEmployees") {
